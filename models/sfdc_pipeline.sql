f46979d0cb01bbe8299db4c784e5580d88bca0ee
Afzal Jasani <amj355@nyu.edu>
2024-05-20 09:24:25 -0700

    update

--- models/sfdc_pipeline.sql
+++ models/sfdc_pipeline.sql
@@ -12,14 +12,13 @@ MODEL (
 
 SELECT
   opportunity_id AS id, /* primary key of opportunity table */
-  opportunity_amount,
   opportunity_name, /* name of the opportunity */
   opportunity_stage, /* stage name from salesforce */
   close_date, /* this is the projected closed date */
   ROUND(opportunity_probability * opportunity_amount, 2) AS expected_amount,
   CASE
     WHEN opportunity_stage = 'demo'
-    THEN 1.99 * opportunity_probability
+    THEN 3.99 * opportunity_probability
     ELSE NULL
   END AS opportunity_probability_new /* custom probability forecasting */
 FROM sqlmesh_example.sfdc_opp_model
